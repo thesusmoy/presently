25a1689d3fdba7e8ada2254467bb96694f3caa89
Susmoy Debnath <susmoydebnath05@gmail.com>
2025-03-28 01:14:54 +0600

    all issues are fixed

--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const http = require('http');
-const { setSocket } = require("./socket/SetSockets");
+const { setSocket } = require('./socket/SetSockets');
 const mainRoute = require('./routes/mainRoute');
 let dotenv = require('dotenv');
-const createConnectionToDataBase = require("./database/connectToMongoDB");
-
+const createConnectionToDataBase = require('./database/connectToMongoDB');
 
 //middlewares
 dotenv.config();
@@ -14,18 +13,18 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-    origin: "*",
-    methods: '*',
-    transport: '*',
+    origin: 'https://presently-os.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use('/api', mainRoute);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('API is running...');
-})
+});
 
 // Create HTTP server
 const server = http.createServer(app);
@@ -33,10 +32,9 @@ const server = http.createServer(app);
 // Set up Socket.io
 const io = new Server(server, {
     cors: corsOptions,
-    path: "/socket.io/",
+    path: '/socket.io/',
 });
 
-
-createConnectionToDataBase()
+createConnectionToDataBase();
 setSocket(io);
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
