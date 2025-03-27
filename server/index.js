29ba3ab1992fbefc9c7479449508ffcae8f84d4b
Susmoy Debnath <susmoydebnath05@gmail.com>
2025-03-28 01:22:28 +0600

    issues fixed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
     origin: 'https://presently-os.vercel.app',
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
